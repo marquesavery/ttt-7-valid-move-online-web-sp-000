a2608cb18b2b307c286431f9723ef8e908c84de9
Marques Avery <averymarques4@gmail.com>
2020-07-21 02:35:55 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if board[index] == "X" || board[index] == "O"
     false
-  else board[index] == " " || board[index] == "" || board[index] == nil
+  else board[index] == " " || board[index] == ""
     true
   end
 end
